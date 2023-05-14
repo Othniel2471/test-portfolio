c1f3592a04f1b526b0e196fd6c791cf71c42a5a1
AnariJnr <anarigodswill@gmail.com>
2023-05-14 04:30:42 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 const portfolio = [
     {
-        id:1,
+        id:0,
         workImg:'assets/images/background/desktop/portfolio image 1.png',
         workTitle:'tonic',
         workRole:{role: 'canopy', job: 'Back End Dev', year: 2015},
@@ -8,7 +8,7 @@ const portfolio = [
         workTools:{stack1:'HTML',stack2:'CSS',stack3:'Javascript'}
     },
     {
-        id:2,
+        id:1,
         class:'two',
         workImg:'assets/images/background/desktop/portfolio image 2.png',
         workTitle:'Multi-Post Stories',
@@ -17,7 +17,7 @@ const portfolio = [
         workTools:{stack1:'HTML',stack2:'Ruby on rails',stack3:'CSS',stack4:'Javascript'}
     },
     {
-        id:3,
+        id:2,
         workImg:'assets/images/background/desktop/portfolio image 3.png',
         workTitle:'facebook 360',
         workRole:{role: 'facebook', job: 'Full Stack  Dev', year: 2015},
@@ -25,7 +25,7 @@ const portfolio = [
         workTools:{stack1:'HTML',stack2:'Ruby on rails',stack3:'CSS',stack4:'Javascript'}
     },
     {
-        id:4,
+        id:3,
         class:'two',
         workImg:'assets/images/background/desktop/portfolio image 4.png',
         workTitle:'uber navigation',
@@ -41,7 +41,7 @@ const modalContainer = document.querySelector('.modal-container')
 
 window.addEventListener('DOMContentLoaded',()=>{
     displayPortfolio(portfolio);
-    console.log(portfolio[1].id);
+    // console.log(portfolio[1].id);
 })
 
 const displayPortfolio = (portfolio)=>{
@@ -74,19 +74,43 @@ const displayPortfolio = (portfolio)=>{
     }).join('')
     sectionPortfolio.innerHTML = displayPortfolio;
 
-    const modalBtn = document.querySelectorAll('.btn')
+
+
+
+    // *Aj says* const modalBtn = document.querySelectorAll('.btn')
     const overlay = document.querySelector(".modal-overlay");
     const closeBtn = document.querySelector(".close-btn");
+   
+    // *Aj says* look in the section portfolio not the document
+    const modalBtn = sectionPortfolio.querySelectorAll('.btn')
 
-    
     // modal functions
     modalBtn.forEach((btn)=>{
-        btn.addEventListener('click',()=>{
+        btn.addEventListener('click',(e)=>{
+            // *Aj says* SELECT THE CURRENT BTN
+            const clickedBtn = e.currentTarget;
+           
+    
+          // *Aj says* got the dataModal of the clicked photo
+
+          const btnDataModal = clickedBtn.dataset.modal;
+
+            // *Aj says* parse the btnDataModal
+        const theParsedDataModal = parseInt(btnDataModal); 
+
+        // *Aj says* created a temp array to hold the particular item we hoverd on
+
+        let tempArray = portfolio.slice(theParsedDataModal, theParsedDataModal +1);
+        displayModalContent(tempArray);
+       
+
+
             let modal = btn.getAttribute('data-modal')
             // const modalContent = portfolio.filter(item => item.id == modal).shift();
             document.getElementById(modal).addEventListener('click',()=>{
                 overlay.classList.add('open-modal')
-                displayModalContent(portfolio)
+                // *Aj says* parse in temp array
+                 displayModalContent(tempArray)
             })
             closeBtn.addEventListener("click", () => {
                 overlay.classList.remove("open-modal");
@@ -127,30 +151,35 @@ const displayPortfolio = (portfolio)=>{
 //     modalContainer.innerHTML = modalContent;
 // }
 
-const displayModalContent = (portfolio)=>{
-    let modalContent = portfolio.map((content)=>{
-        return `
-     <div class="modal-project">
-     <h2 class="work-title">${content.workTitle}</h2>
-     <ul class="canopy">
-       <li>${content.workRole.role}</li>
-       <li>${content.workRole.job}</li>
-       <li>${content.workRole.year}</li>
-     </ul>
-     <div class="work-img">
-       <img src="${content.workImg}" alt="tonic image">
-     </div>
-     <div class="project-description">
-       <p>${content.workDetails}</p>
-       <ul class="work-tools">
-         <li>${content.workTools.stack1}</li>
-         <li>${content.workTools.stack2}</li>
-         <li>${content.workTools.stack3}</li>
-         ${content.workTools.stack4 ? `<li>${content.workTools.stack4}</li>` : ''}
-       </ul>
-     </div>
-   </div>     
-    `
-    }).join('')
-    modalContainer.innerHTML = modalContent
+
+
+// *Aj says* rewrited displayModalContent fuc
+const displayModalContent = (anArray)=>{
+    anArray = anArray.map(function(content){
+        return ` <div class="modal-project">
+        <h2 class="work-title">${content.workTitle}</h2>
+        <ul class="canopy">
+          <li>${content.workRole.role}</li>
+          <li>${content.workRole.job}</li>
+          <li>${content.workRole.year}</li>
+        </ul>
+        <div class="work-img">
+          <img src="${content.workImg}" alt="tonic image">
+        </div>
+        <div class="project-description">
+          <p>${content.workDetails}</p>
+          <ul class="work-tools">
+            <li>${content.workTools.stack1}</li>
+            <li>${content.workTools.stack2}</li>
+            <li>${content.workTools.stack3}</li>
+            ${content.workTools.stack4 ? `<li>${content.workTools.stack4}</li>` : ''}
+          </ul>
+        </div>
+      </div> `
+    }).join("");
+    console.log(anArray);
+    modalContainer.innerHTML = anArray;
+   
 }
+
+// *Aj says* it costs a 100$ for this
